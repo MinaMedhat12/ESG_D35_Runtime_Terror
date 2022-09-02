b2d6cb7cecd6e07b704f99cfc26dbd8bb2d85393
samira saeid <samirasaeid264@gmail.com>
2022-09-02 13:50:30 +0200

    last modification

--- Samira Saeid/linked_node.c
+++ Samira Saeid/linked_node.c
@@ -8,7 +8,7 @@ struct node{
     struct node *link;
 };
 
-void insertNode(struct node* last, int value);
+void insertNode(struct node* head, struct node* prev_node, int value);
 void addLast(struct node *head, int val);
 void delLast(struct node* head);
 void dell(struct node* head,u8_t pos);
@@ -43,7 +43,7 @@ int main(){
     scanf("%d",&num);
 
     if(num == 1){
-        insertNode(third,88);
+        insertNode(head,third,88);
     }
     else if(num == 2){
         addLast(head,60);
@@ -63,18 +63,26 @@ int main(){
     }
     else{}
 
-    //printf("%d",->data);
 
     return 0;
 }
-void insertNode(struct node* prev_node, int value){
+void insertNode(struct node* head, struct node* prev_node, int value){
+    
     struct node* next =NULL;
     next = (struct node*) malloc(sizeof(struct node));
 
     next->data = value;
     prev_node->link = next;
     next->link =NULL;
-    printf("%d",next->data);
+
+    struct node *temp = head;
+            while(temp != NULL)
+            {
+            printf("%d->",temp->data);
+            temp = temp->link;
+            }
+            printf("Null");
+    
 
     return;
 
@@ -89,15 +97,28 @@ void addLast(struct node *head, int val){
     end->link =NULL;
 
     while(ptr->link != NULL){
+        
         ptr = ptr->link;
+       
     }
     ptr->link = end;
 
-    printf("%d\n",end->data);
+
+    struct node *temp = head;
+    while(temp != NULL)
+  {
+      printf("%d->",temp->data);
+      temp = temp->link;
+  }
+   printf("Null");
+    
+
+    
 
     return;
 
 }
+
 void delLast(struct node* head){
     if(head == NULL){
         printf("List is empty");
@@ -115,14 +136,24 @@ void delLast(struct node* head){
             ptr = ptr->link;
         }
         ptr2-> link = NULL;
+        
         printf("the delete data is %d\n",ptr->data);
         free(ptr);
-        printf("%d\n",ptr->data);
+        
+
+            struct node *temp = head;
+            while(temp != NULL)
+            {
+            printf("%d->",temp->data);
+            temp = temp->link;
+            }
+            printf("Null");
 
         return ;
 
     }
-    }
+}
+
     void dell(struct node* head,u8_t pos){
         
         struct node* ptr = head;
@@ -145,10 +176,17 @@ void delLast(struct node* head){
 
             }
             ptr1->link = ptr->link;
-            printf("%d\n",ptr->data);
+            printf("The deleted node's data is is %d\n",ptr->data);
             free(ptr);
-            ptr = NULL;
-            printf("%d\n",ptr->data);
+
+            struct node *temp = head;
+            while(temp != NULL)
+            {
+            printf("%d->",temp->data);
+            temp = temp->link;
+            }
+            printf("Null");
+            
         }
            
         return;
@@ -167,7 +205,15 @@ void delLast(struct node* head){
         }
         ptr2->link = ptr->link;
         ptr->link = ptr2;
-        printf("%d",ptr2->data);
+
+        struct node *temp = head;
+            while(temp != NULL)
+            {
+            printf("%d->",temp->data);
+            temp = temp->link;
+            }
+            printf("Null");
+        
 
 
          
